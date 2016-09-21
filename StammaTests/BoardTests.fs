ee576196488cb9e33e0d566d18b96ac792e21dac
asibahi <a.sibahi@gmail.com>
2016-09-21 15:13:42 +0300

    Started BoardTests unit test

---
+++ StammaTests/BoardTests.fs
@@ -0,0 +1,17 @@
+﻿module StammaTests.BoardTests
+
+open Stamma
+open NUnit.Framework
+open FsUnitTyped
+
+[<Test>]
+let ``ofFen for empty board - should succeed``() = 
+    let fen = "10/10/10/10/10/10/10/10/10/10"
+    let board = Board.ofFen fen
+    for r in 1..10 do
+        for f in 1..10 do
+            let field = 
+                board |> Map.find { Rank = r
+                                    File = f }
+            field |> shouldEqual Empty
+            board.Count |> shouldEqual 100
