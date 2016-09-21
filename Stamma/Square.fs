fa732e3c18180bb21dba136c2aa87453310ddafa
asibahi <a.sibahi@gmail.com>
2016-09-21 22:01:11 +0300

    Added Square type and Algebraic conversions

---
+++ Stamma/Square.fs
@@ -0,0 +1,9 @@
+﻿[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+[<RequireQualifiedAccess>]
+module Stamma.Square
+
+let ofAlg (str : string) = 
+    { Rank = int str.[0] - 96
+      File = str.Substring 1 |> int }
+
+let toAlg sq = sprintf "%c%d" (sq.Rank + 96 |> char) sq.File
