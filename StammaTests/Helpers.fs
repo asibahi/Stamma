6ac2db8f922b33dc77e31566aa269ca219041b2e
asibahi <a.sibahi@gmail.com>
2016-09-21 14:59:45 +0300

    Added Piece unit tests

---
+++ StammaTests/Helpers.fs
@@ -0,0 +1,21 @@
+﻿module StammaTests.Helpers
+
+open Stamma
+
+let toColor = 
+    function 
+    | "White" -> White
+    | "Black" -> Black
+    | _ -> failwith "Invalid"
+
+let toPiece = 
+    function 
+    | "King" -> King
+    | "Queen" -> Queen
+    | "Marhsal" -> Marshal
+    | "Cardinal" -> Cardinal
+    | "Knight" -> Knight
+    | "Bishop" -> Bishop
+    | "Rook" -> Rook
+    | "Pawn" -> Pawn
+    | _ -> failwith "Invalid"
