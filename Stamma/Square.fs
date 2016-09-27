bed6a915f151759ed1580a7cd9f37552be2b4bce
asibahi <a.sibahi@gmail.com>
2016-09-27 10:42:46 +0300

    Added validity checking.

--- Stamma/Square.fs
+++ Stamma/Square.fs
@@ -3,7 +3,13 @@
 module Stamma.Square
 
 let ofAlg (str : string) = 
-    { Rank = str.Substring 1 |> int
+    let str = str.ToLowerInvariant()
+    if "abcdefghij".IndexOf str.[0] = -1 then failwith "Invalid file"
+    let rnk = str.Substring 1 |> int
+    if rnk < 1 || rnk > 10 then failwith "Invalid rank"
+    { Rank = rnk
       File = int str.[0] - 96 }
 
-let toAlg sq = sprintf "%c%d" (sq.File + 96 |> char) sq.Rank
+let toAlg sq = 
+    if sq.Rank < 1 || sq.Rank > 10 || sq.File < 1 || sq.File > 10 then failwith "Invalid coordinates"
+    sprintf "%c%d" (sq.File + 96 |> char) sq.Rank
