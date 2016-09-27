2340e634fe5efc70ff895939e7754d4bb7510126
asibahi <a.sibahi@gmail.com>
2016-09-27 10:42:36 +0300

    Added unit tests to test legality, check, etc. work in progress

--- StammaTests/PositionTests.fs
+++ StammaTests/PositionTests.fs
@@ -4,6 +4,7 @@ open Stamma
 open NUnit.Framework
 open FsUnitTyped
 
+(* ofFen Tests *)
 [<Test>]
 let ``ofFen starting position``() = 
     let pos = Position.ofFen "r8r/1nbqkmcbn1/pppppppppp/10/10/10/10/PPPPPPPPPP/1NBQKMCBN1/R8R w - 0"
@@ -53,8 +54,78 @@ let ``ofFen KQPK : Pal Benko composition Mate in 2``() =
     pos.CappedWhite |> shouldNotContain Queen
     pos.CappedBlack |> shouldHaveLength 9
 
-[<Test>]
-let ``isCheckmate Fool's mate - should be true (obviously)``() = 
-    Position.ofFen "r4q3r/1nb1kmcbn1/ppCppppppp/10/10/10/10/PPPPPPPPPP/1NBQKM1BN1/R8R b - 0"
+(* isCheck tests *)
+
+[<TestCase("r4q3r/1nb1kmcbn1/ppCppppppp/10/10/10/10/PPPPPPPPPP/1NBQKM1BN1/R8R b - 0")>]
+[<TestCase("9k/10/7C2/10/10/9K/10/10/10/10 b - 0")>]
+[<TestCase("5k4/R9/5M4/10/5K4/10/10/10/10/10 b - 0")>]
+[<TestCase("5K4/r9/5m4/10/5k4/10/10/10/10/10 w - 0")>]
+[<TestCase("9k/9R/7N2/10/K9/10/10/10/10/10 b - 0")>]
+// fool's mate
+// Cardinal mate
+// Rook and marshal
+// Rook and marshal opposite colors
+// Arabian mate
+let ``isCheck tests - checkmate input - should be true`` (str) = 
+    Position.ofFen str
+    |> Position.isCheck
+    |> shouldEqual true
+
+(* isLegal tests *)
+
+[<TestCase("10/10/5K4/5k4/10/5p4/5P4/10/10/10 w - 0")>]
+[<TestCase("10/10/10/4Kk4/10/5p4/5P4/10/10/10 b - 0")>]
+[<TestCase("R3k5/10/5K4/10/10/10/10/10/10/10 w - 0")>]
+[<TestCase("r3K5/10/5k4/10/10/10/10/10/10/10 b - 0")>]
+// Kings touching
+// Kings touching
+// Black in check white to move
+// White in check black to move
+let ``isLegal tests - illegal positions - should be false`` (str) = 
+    Position.ofFen str
+    |> Position.isLegal
+    |> shouldEqual false
+
+[<TestCase("r4q3r/1nb1kmcbn1/ppCppppppp/10/10/10/10/PPPPPPPPPP/1NBQKM1BN1/R8R b - 0")>]
+[<TestCase("9k/10/7C2/10/10/9K/10/10/10/10 b - 0")>]
+[<TestCase("5k4/R9/5M4/10/5K4/10/10/10/10/10 b - 0")>]
+[<TestCase("5K4/r9/5m4/10/5k4/10/10/10/10/10 w - 0")>]
+[<TestCase("9k/9R/7N2/10/K9/10/10/10/10/10 b - 0")>]
+let ``isLegal tests - checkmate positions - should be true`` (str) = 
+    Position.ofFen str
+    |> Position.isLegal
+    |> shouldEqual true
+
+(* isCheckmate tests *)
+
+[<TestCase("r4q3r/1nb1kmcbn1/ppCppppppp/10/10/10/10/PPPPPPPPPP/1NBQKM1BN1/R8R b - 0")>]
+[<TestCase("9k/10/7C2/10/10/9K/10/10/10/10 b - 0")>]
+[<TestCase("5k4/R9/5M4/10/5K4/10/10/10/10/10 b - 0")>]
+[<TestCase("5K4/r9/5m4/10/5k4/10/10/10/10/10 w - 0")>]
+[<TestCase("9k/9R/7N2/10/K9/10/10/10/10/10 b - 0")>]
+let ``isCheckmate tests - actual checkmates input - should be true`` (str) = 
+    Position.ofFen str
     |> Position.isCheckmate
     |> shouldEqual true
+
+[<TestCase("5k4/5P4/5K4/10/10/10/10/10/10/10 b - 0")>]
+[<TestCase("5k4/10/5P4/5K4/10/10/10/10/10/10 b - 0")>]
+[<TestCase("5k4/5P4/5K4/10/10/10/10/10/10/10 w - 0")>]
+[<TestCase("5k4/10/5P4/5K4/10/10/10/10/10/10 w - 0")>]
+let ``isCheckmate tests - no check input - should be false`` (str) = 
+    Position.ofFen str
+    |> Position.isCheckmate
+    |> shouldEqual false
+
+(* isStalemate test *)
+[<Test>]
+let ``isStalemate tests - classic stalemate - should be true``() = 
+    Position.ofFen "5k4/5P4/5K4/10/10/10/10/10/10/10 b - 0"
+    |> Position.isStalemate
+    |> shouldEqual true
+
+[<Test>]
+let ``isStalemate tests - not stalemate - should be false``() = 
+    Position.ofFen "5k4/10/5P4/5K4/10/10/10/10/10/10 b - 0"
+    |> Position.isStalemate
+    |> shouldEqual false
